17129139097d98783117363e3b90bbd2bf69cc82
Sergei Ivankov <sergeiivankov@yandex.ru>
2019-01-21 21:59:31 +0300

    Наведение парядка в директории controllers

--- controllers/index.php
+++ controllers/index.php
@@ -12,19 +12,19 @@ class Controller_Index extends Controller_Base {
     $main_config = Config::get('main');
 
     // Установка глобальной переменной вида title
-    // Она используется как в базовом шаблоне вида, так и в шаблоне вида контента
+    // Она используется как в базовом шаблоне вида,
+    // так и в шаблоне вида контента
     View::set_global('title', $main_config['title']);
 
     // Создание экземпляра класса вида контента
-    // Вторым параметром передается массив локальных переменных,
-    // где ключ - название переменной, значение - значение переменной
+    // Вторым параметром передается массив локальных переменных
     $content_view = View::factory('content', [
       'text1' => $main_config['description'],
-      'text2' => '<a href="https://github.com/xooler/xs-php">https://github.com/xooler/xs-php</a>'
+      'text2' => '<a href="https://github.com/xooler/xs-php">xooler/xs-php</a>'
     ]);
 
-    // Устанавливаем экземпляр класса вида контента
-    // в свойство content родительского контроллера базового шаблона
+    // Устанавливаем экземпляр класса вида в свойство content
+    // для вставки его в шаблок (см. /controllers/template.php)
     $this->content = $content_view;
   }
 }
